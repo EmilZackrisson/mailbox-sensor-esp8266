a6152236ec1f754ffaee11137a8a319a76b0b8aa
Emil Zackrisson <emil.zackrisson@gmail.com>
2020-08-10 22:55:10 +0200

    Update brevlada_sensor.ino

--- brevlada_sensor/brevlada_sensor.ino
+++ brevlada_sensor/brevlada_sensor.ino
@@ -7,6 +7,7 @@ const int power = 4; // D2
 
 ADC_MODE(ADC_VCC);
 int Batt;
+int timesRun;
 
 //Enter your mqtt server configurations
 const char* mqttServer = "192.168.0.56";    //Enter Your mqttServer address
@@ -50,35 +51,48 @@ void setup() {
       Serial.print("Cound not connect to MQTT");
       Serial.println(client.state());  //If you get state 5: mismatch in configuration
       delay(2000);
-
+      digitalWrite(power, LOW); // Turn ESP off
+      delay(500);
     }
   }
 }
 
 void loop() {
-  client.loop();
+  if (timesRun <= 3) {
+    client.loop();
 
-  Batt = ESP.getVcc() / 10;
-  char buf[4];
-  sprintf (buf, "%03i", Batt);
+    Batt = ESP.getVcc() / 10;
+    char buf[4];
+    sprintf (buf, "%03i", Batt);
 
-  char* openMessage = "on";
-  int openLength = strlen(openMessage);
-  boolean retained = true;
-  client.publish("brevlada/state", (byte*)openMessage, openLength, retained);
+    char* openMessage = "on";
+    int openLength = strlen(openMessage);
+    boolean retained = true;
+    client.publish("brevlada/state", (byte*)openMessage, openLength, retained);
+    Serial.println("on");
 
+    char* batteryMessage = buf;
+    int battLength = strlen(batteryMessage);
+    client.publish("brevlada/battery", (byte*)batteryMessage, battLength, retained);
+    Serial.println(batteryMessage);
+    delay(2000);
 
-  char* batteryMessage = buf;
-  int battLength = strlen(batteryMessage);
-  client.publish("brevlada/battery", (byte*)batteryMessage, battLength, retained);
+    char* closedMessage = "off";
+    int closedLength = strlen(closedMessage);
+    client.publish("brevlada/state", (byte*)closedMessage, closedLength, retained);
+    Serial.println("off");
 
-  delay(2000);
+    delay(500);
+    digitalWrite(power, LOW); // Turn ESP off
+    timesRun++;
+    Serial.println(timesRun);
+    delay(10000);
+  }
 
-  char* closedMessage = "off";
-  int closedLength = strlen(closedMessage);
-  client.publish("brevlada/state", (byte*)closedMessage, closedLength, retained);
-
-
-  delay(500);
-  digitalWrite(power, LOW); // Turn ESP off
+  if (timesRun >= 3) {
+    Serial.println("Going to sleep");
+    WiFi.disconnect();
+    WiFi.forceSleepBegin();
+    delay(1); //For some reason the modem won't go to sleep unless you do a delay
+  }
 }
